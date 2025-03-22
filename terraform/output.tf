b77ac66f7d9dab776fb069a4e8635fbf15d0ca5e
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-03-22 17:17:52 -0400

    test

---
+++ terraform/output.tf
@@ -0,0 +1,4 @@
+output "amplify_app_url" {
+  value = "https://${aws_amplify_branch.main.branch_name}.${aws_amplify_app.plaid_app.default_domain}"
+  description = "URL to access the deployed Amplify app"
+}
