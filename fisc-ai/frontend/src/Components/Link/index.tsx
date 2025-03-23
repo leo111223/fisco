3169cf03f4b93ac0fc704095fcbbcce0a84361b6
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-03-23 02:55:13 -0400

    test

--- fisc-ai/frontend/src/Components/Link/index.tsx
+++ fisc-ai/frontend/src/Components/Link/index.tsx
@@ -1,61 +1,55 @@
 import React, { useEffect, useContext } from "react";
 import { usePlaidLink } from "react-plaid-link";
 import Button from "plaid-threads/Button";
-
 import Context from "../../Context";
 
 const Link = () => {
-  const { linkToken, isPaymentInitiation, isCraProductsExclusively, dispatch } =
-    useContext(Context);
+  const { linkToken, isPaymentInitiation, isCraProductsExclusively, dispatch } = useContext(Context);
 
-  const onSuccess = React.useCallback(
-    (public_token: string) => {
-      // If the access_token is needed, send public_token to server
-      const exchangePublicTokenForAccessToken = async () => {
-        const response = await fetch("/api/set_access_token", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-          },
-          body: `public_token=${public_token}`,
-        });
-        if (!response.ok) {
-          dispatch({
-            type: "SET_STATE",
-            state: {
-              itemId: `no item_id retrieved`,
-              accessToken: `no access_token retrieved`,
-              isItemAccess: false,
-            },
-          });
-          return;
-        }
-        const data = await response.json();
+  const onSuccess = React.useCallback((public_token: string) => {
+    const exchangePublicTokenForAccessToken = async () => {
+      const apiUrl = `${process.env.REACT_APP_API_URL}/set_access_token`;
+
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ public_token }),
+      });
+
+      if (!response.ok) {
         dispatch({
           type: "SET_STATE",
           state: {
-            itemId: data.item_id,
-            accessToken: data.access_token,
-            isItemAccess: true,
+            itemId: `no item_id retrieved`,
+            accessToken: `no access_token retrieved`,
+            isItemAccess: false,
           },
         });
-      };
-
-      // 'payment_initiation' products do not require the public_token to be exchanged for an access_token.
-      if (isPaymentInitiation) {
-        dispatch({ type: "SET_STATE", state: { isItemAccess: false } });
-      } else if (isCraProductsExclusively) {
-        // When only CRA products are enabled, only user_token is needed. access_token/public_token exchange is not needed.
-        dispatch({ type: "SET_STATE", state: { isItemAccess: false } });
-      } else {
-        exchangePublicTokenForAccessToken();
+        return;
       }
 
-      dispatch({ type: "SET_STATE", state: { linkSuccess: true } });
-      window.history.pushState("", "", "/");
-    },
-    [dispatch, isPaymentInitiation, isCraProductsExclusively]
-  );
+      const data = await response.json();
+      dispatch({
+        type: "SET_STATE",
+        state: {
+          itemId: data.item_id,
+          accessToken: data.access_token,
+          isItemAccess: true,
+        },
+      });
+    };
+
+    if (isPaymentInitiation || isCraProductsExclusively) {
+      dispatch({ type: "SET_STATE", state: { isItemAccess: false } });
+    } else {
+      exchangePublicTokenForAccessToken();
+    }
+
+    dispatch({ type: "SET_STATE", state: { linkSuccess: true } });
+    window.history.pushState("", "", "/");
+  }, [dispatch, isPaymentInitiation, isCraProductsExclusively]);
 
   let isOauth = false;
   const config: Parameters<typeof usePlaidLink>[0] = {
@@ -64,7 +58,6 @@ const Link = () => {
   };
 
   if (window.location.href.includes("?oauth_state_id=")) {
-    // TODO: figure out how to delete this ts-ignore
     // @ts-ignore
     config.receivedRedirectUri = window.location.href;
     isOauth = true;
@@ -86,5 +79,4 @@ const Link = () => {
 };
 
 Link.displayName = "Link";
-
 export default Link;
