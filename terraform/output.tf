e2da9999a81f4642f73f831dd884cc7b4dc7b2a7
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-03-23 02:01:53 -0400

    test

--- terraform/output.tf
+++ terraform/output.tf
@@ -12,3 +12,4 @@ output "api_gateway_url" {
   value = "https://${aws_api_gateway_rest_api.finance_api.id}.execute-api.${var.aws_region}.amazonaws.com/prod"
   description = "Root URL for API Gateway"
 }
+
