9d4b3ff355fb13e3cd7fda0e6bf25a3ea93e9749
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-03-22 23:46:11 -0400

    test

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@ output "amplify_app_url" {
   value = "https://${aws_amplify_branch.main_branch.branch_name}.${aws_amplify_app.plaid_app.default_domain}"
   description = "URL to access the deployed Amplify app"
 }
+
+output "api_gateway_url" {
+  description = "Invoke this endpoint from the Amplify React app"
+  value       = "https://${aws_api_gateway_rest_api.finance_api.id}.execute-api.${var.aws_region}.amazonaws.com/prod/transactions"
+}
