3a285d9e012765bda82ab3054d9ee148e4438295
Devang Dhanuka <devangdhanuka@gmail.com>
2025-03-23 23:35:21 -0400

    update terraform code

--- dhruv-backend/backend/api/app/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .items import router as items_router
-from .tokens import router as tokens_router
-from .webhook import router as webhook_router
-
-__all__ = ["items_router", "tokens_router", "webhook_router"]
