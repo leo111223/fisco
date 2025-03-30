3d5094da0348666f590203d703de9d3d057ee86f
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-03-30 00:51:56 -0400

    fetch url

--- fisc-ai/frontend/src/App.tsx
+++ fisc-ai/frontend/src/App.tsx
@@ -22,6 +22,7 @@ Amplify.configure(awsconfig);
 // const API_BASE_URL = "https://7o81y9tcsa.execute-api.us-east-1.amazonaws.com/dev";
 //const API_BASE_URL = "https://rf59517zr9.execute-api.us-east-1.amazonaws.com/prod";
 
+console.log(import.meta.env)
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;     //leo
 
