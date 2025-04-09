9c5647686d6e93f7efec2e3d243d970a94a40d3b
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-04-09 19:04:56 -0400

    leo

--- terraform/lex_api_updated.tf
+++ terraform/lex_api_updated.tf
@@ -108,14 +108,6 @@ resource "aws_api_gateway_integration_response" "query_lex_options_integration_r
   ]
 }
 
-resource "aws_api_gateway_integration" "lex_integration" {
-  rest_api_id = aws_api_gateway_rest_api.lex_api.id
-  resource_id = aws_api_gateway_resource.lex_resource.id
-  http_method = aws_api_gateway_method.lex_method.http_method
-  integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = aws_lambda_function.transaction_handler.invoke_arn
-}
 
 resource "aws_lambda_permission" "apigw_lex_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
