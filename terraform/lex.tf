50284f22597e054fb0d269de0d8407d866f4e833
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-04-10 00:31:34 -0400

    leo

--- terraform/lex.tf
+++ terraform/lex.tf
@@ -47,23 +47,7 @@ resource "aws_lexv2models_bot" "finance_assistant" {
 
 resource "null_resource" "create_lex_alias" {
   provisioner "local-exec" {
-    command = <<EOT
-      set -e
-
-      VERSION=$(aws lexv2-models create-bot-version \
-        --bot-id ${aws_lexv2models_bot.finance_assistant.id} \
-        --locale-id en_US \
-        --query 'botVersion' \
-        --output text)
-
-      JSON=$(jq -n --arg version "$VERSION" '{"en_US": {"sourceBotVersion": $version}}')
-
-      aws lexv2-models create-bot-alias \
-        --bot-id ${aws_lexv2models_bot.finance_assistant.id} \
-        --bot-alias-name "financeAssistantAlias" \
-        --bot-version-locale-specification "$JSON"
-    EOT
-    interpreter = ["bash", "-c"]
+    command = "./create_lex_alias.sh ${aws_lexv2models_bot.finance_assistant.id}"
   }
 
   triggers = {
@@ -72,6 +56,7 @@ resource "null_resource" "create_lex_alias" {
 
   depends_on = [aws_lexv2models_bot_locale.english_locale]
 }
+
 
 
 
