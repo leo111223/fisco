a384bd5c181c29fbfa97e2593974a64e7c79de9b
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-04-12 21:09:32 -0400

    main

--- terraform/lex.tf
+++ terraform/lex.tf
@@ -86,13 +86,14 @@ resource "aws_lambda_permission" "allow_apigw_invoke_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.query_lex_handler.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.finance_api.id}/*/POST/query_lex"
+  source_arn    = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.finance_api.id}/prod/POST/query_lex"
     
   
 
   depends_on = [
-  aws_lambda_function.query_lex_handler,
-  aws_api_gateway_deployment.api_deployment
+    aws_lambda_function.query_lex_handler,
+    aws_api_gateway_deployment.api_deployment,
+    aws_api_gateway_stage.api_stage
   ]
 }
 
