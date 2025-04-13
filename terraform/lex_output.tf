32467d2ff36529e586293cab18b7230af48b1dbe
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-04-12 22:49:49 -0400

    main

--- terraform/lex_output.tf
+++ terraform/lex_output.tf
@@ -1,12 +1,28 @@
+# data "external" "lex_alias_id" {
+#   program = ["bash", "-c", <<EOT
+#     aws lexv2-models list-bot-aliases \
+#       --bot-id ${aws_lexv2models_bot.finance_assistant.id} \
+#       --query "botAliasSummaries[?botAliasName=='financeAssistantAlias'].botAliasId | [0]" \
+#       --output json | jq -nR '{lex_bot_alias_id: input}'
+#     EOT
+#   ]
+# depends_on = [
+#     null_resource.create_lex_alias
+#   ]
+# }
+
 data "external" "lex_alias_id" {
   program = ["bash", "-c", <<EOT
-    aws lexv2-models list-bot-aliases \
+    ALIAS_ID=$(aws lexv2-models list-bot-aliases \
       --bot-id ${aws_lexv2models_bot.finance_assistant.id} \
       --query "botAliasSummaries[?botAliasName=='financeAssistantAlias'].botAliasId | [0]" \
-      --output json | jq -nR '{lex_bot_alias_id: input}'
-    EOT
+      --output text)
+
+    echo "{\"lex_bot_alias_id\": \"$ALIAS_ID\"}"
+  EOT
   ]
-depends_on = [
+
+  depends_on = [
     null_resource.create_lex_alias
   ]
 }
