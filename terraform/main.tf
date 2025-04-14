b03e40b2f8c9afc87a123aa75e8b95ecc87fe7d2
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-04-14 18:08:48 -0400

    main

--- terraform/main.tf
+++ terraform/main.tf
@@ -170,7 +170,7 @@ resource "aws_dynamodb_table" "accounts" {
 
 # S3 for receipts
 resource "aws_s3_bucket" "fisc_ai_receipt_bucket" {
-  bucket         = "fisc-ai-receipts-123"
+  bucket         = "fisc-ai-receipts"
   force_destroy  = true
 
   lifecycle {
