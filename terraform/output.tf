169027cd2bf95bf0319670d66ff5718a07beb3c3
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-04-15 00:04:29 -0400

    main

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,18 +4,18 @@ output "amplify_app_url" {
 }
 
 
-output "api_gateway_url" {
-  value = "https://${aws_api_gateway_rest_api.finance_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}"
-  description = "URL to access the deployed API Gateway"
-}
+# output "api_gateway_url" {
+#   value = "https://${aws_api_gateway_rest_api.finance_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}"
+#   description = "URL to access the deployed API Gateway"
+# }
 
 output "s3_bucket" {
   value = aws_s3_bucket.receipt_bucket.id
 }
 
-output "apigw_arn" {
-  value = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.finance_api.id}/prod/POST/query_lex"
-}
+# output "apigw_arn" {
+#   value = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.finance_api.id}/prod/POST/query_lex"
+# }
 
 
 # output "resolved_lex_alias_id" {
