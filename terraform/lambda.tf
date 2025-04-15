539197eb7bd35001361a0fbc8b9c2ef9e2d022e3
leo111223 <75706742+leo111223@users.noreply.github.com>
2025-04-15 02:40:27 -0400

    main

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -78,22 +78,22 @@ resource "aws_lambda_function" "get_accounts_handler" {
 }
 
 
-# Fetch Transactions Lambda
-resource "aws_lambda_function" "fetch_transactions_handler" {
-  function_name = "fetch_transactions_handler"
-  filename      = "fetch_transactions.zip"  # Ensure this is the zipped deployment package
-  handler       = "fetch_transactions.lambda_handler"  # Update with the handler function in your script
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_exec.arn
-  timeout       = 30
+# # Fetch Transactions Lambda
+# resource "aws_lambda_function" "fetch_transactions_handler" {
+#   function_name = "fetch_transactions_handler"
+#   filename      = "fetch_transactions.zip"  # Ensure this is the zipped deployment package
+#   handler       = "fetch_transactions.lambda_handler"  # Update with the handler function in your script
+#   runtime       = "python3.9"
+#   role          = aws_iam_role.lambda_exec.arn
+#   timeout       = 30
 
-  environment {
-    variables = {
-      STAGE = "prod"
-      DYNAMODB_TABLE = aws_dynamodb_table.transactions.name
-    }
-  }
-}
+#   environment {
+#     variables = {
+#       STAGE = "prod"
+#       DYNAMODB_TABLE = aws_dynamodb_table.transactions.name
+#     }
+#   }
+# }
 
 
 # # Textract Receipt Lambda
